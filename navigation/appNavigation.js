9fbd26772339d7fa82aeac57f9e0160aaf613645
arnav chaudhary <arnav@arnavs-MacBook-Pro-2.local>
2023-10-10 20:57:03 +0530

    arnav pc changes

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,56 +1,47 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen.js';
 import OnboardingScreen from '../screens/OnboardingScreen.js';
 import { getItem } from '../utils/asyncStorage.js';
 
-
 const Stack = createNativeStackNavigator();
 
-
 export default function AppNavigation() {
-
   const [showOnboarding, setShowOnboarding] = useState(null);
-  useEffect(()=>{
+  useEffect(() => {
     checkIfAlreadyOnboarded();
-  },[])
+  }, []);
 
-  const checkIfAlreadyOnboarded = async ()=>{
+  const checkIfAlreadyOnboarded = async () => {
     let onboarded = await getItem('onboarded');
-    if(onboarded==1){
+    if (onboarded == 1) {
       // hide onboarding
       setShowOnboarding(false);
-    }else{
+    } else {
       // show onboarding
       setShowOnboarding(true);
     }
-  }
+  };
 
-  if(showOnboarding==null){
+  if (showOnboarding == null) {
     return null;
   }
 
-
-  if(showOnboarding){
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Onboarding'>
-          <Stack.Screen name="Onboarding" options={{headerShown: false}} component={OnboardingScreen} />
-          <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    )
-  }else{
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Home'>
-          <Stack.Screen name="Onboarding" options={{headerShown: false}} component={OnboardingScreen} />
-          <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    )
-  }
-
-  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen
+          name="Onboarding"
+          options={{ headerShown: false }}
+          component={OnboardingScreen}
+        />
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,107 +1,113 @@
-import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Lottie from 'lottie-react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { removeItem } from '../utils/asyncStorage';
+import wifi from 'react-native-android-wifi';
 
-const {width, height} = Dimensions.get('window');
+let { width, height } = Dimensions.get('window');
 
 export default function HomeScreen() {
-  const navigation = useNavigation();
+  let navigation = useNavigation();
 
-  const handleReset = async ()=>{
+  let handleReset = async () => {
     await removeItem('onboarded');
     navigation.push('Onboarding');
-  }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <TouchableOpacity onPress={handleReset} style={styles.onboardingButton}>
-        <Text>Go back to <Text style={styles.onboardingBold}>onboarding</Text></Text>
+        <Text>
+          Go back to <Text style={styles.onboardingBold}>onboarding</Text>
+        </Text>
       </TouchableOpacity>
-      <View style = {styles.heading}>
-      <Text style={styles.text}>SleepTracker</Text>
-      <Text>Welcome to the dashboard</Text>
+      <View style={styles.heading}>
+        <Text style={styles.text}>SleepTracker</Text>
+        <Text>Welcome to the dashboard</Text>
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Connect To Device</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button}>
-          <Text style = {styles.buttonText}>Add Audio Alarm</Text>
+          <Text style={styles.buttonText}>Add Audio Alarm</Text>
         </TouchableOpacity>
-        <TouchableOpacity style = {styles.button}>
-          <Text style= {styles.buttonText}>Download Data</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Download Data</Text>
         </TouchableOpacity>
       </View>
       <View>
-        <Text style={styles.footer}>Designed at <Text style={styles.bss}>BSS</Text></Text>
+        <Text style={styles.footer}>
+          Designed at <Text style={styles.bss}>BSS</Text>
+        </Text>
       </View>
     </SafeAreaView>
-  )
+  );
 }
 
-const styles = StyleSheet.create({
+let styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent : 'space-evenly'
+    justifyContent: 'space-evenly',
   },
-  lottie:{
-    width: width*0.9,
-    height: width
+  lottie: {
+    width: width * 0.9,
+    height: width,
   },
-  heading : {
-    marginTop : 60,
-    justifyContent : 'center',
-    alignItems : 'center',
+  heading: {
+    marginTop: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   text: {
-    fontWeight : 'bold',
-    fontSize: width*0.09,
-    marginTop : 80,
-  
+    fontWeight: 'bold',
+    fontSize: width * 0.09,
+    marginTop: 80,
   },
-  buttonContainer : {
-    marginTop : 100,
-    marginBottom : 250,
-    justifyContent : 'center',
-    alignItems : 'center',
-
-    
+  buttonContainer: {
+    marginTop: 100,
+    marginBottom: 250,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  button : {
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 12,
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: 'black',
-    width : 400,
-    height : 50,
-    marginBottom : 40, 
+    backgroundColor: '#000000',
+    width: 400,
+    height: 50,
+    marginBottom: 40,
   },
-  buttonText : {
+  buttonText: {
     fontSize: 16,
     lineHeight: 21,
     fontWeight: 'semibold',
     letterSpacing: 0.25,
     color: 'white',
   },
-  footer : {
-    marginBottom : 20,
+  footer: {
+    marginBottom: 20,
   },
-  bss : {
-    fontWeight : 'bold',
+  bss: {
+    fontWeight: 'bold',
   },
-  onboardingButton : {
-    marginTop : 60,
-
+  onboardingButton: {
+    marginTop: 60,
   },
-  onboardingBold : {
-    fontWeight : 'bold',
-  }
-})
+  onboardingBold: {
+    fontWeight: 'bold',
+  },
+});
